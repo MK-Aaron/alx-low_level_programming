ea02e3fc2bf32481f3deaf4e5ae447a28073ed91
MK-Aaron <ronimarvell@gmail.com>
2023-02-17 14:46:55 -0800

    prints a number depending on conditios

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * prints if a number is equal, positive or negative
+ * return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("%i greater than 0\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d equal to 0\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("%i less than 0\n", n);
+	}
+	return (0);
+}
