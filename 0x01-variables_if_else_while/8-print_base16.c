cb8ffd5cfcb8371f24c23ee5b17a9f7ba24da308
MK-Aaron <ronimarvell@gmail.com>
2023-02-19 05:53:10 -0800

    print numbers and letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print base 16 numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch, n;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	putchar(ch);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+}
