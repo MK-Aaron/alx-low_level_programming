18f8d9eb21a84f3f1a3cbbc0ea81bc7944ae060d
MK-Aaron <ronimarvell@gmail.com>
2023-02-20 06:55:37 -0800

    printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\"");
+	return (0);
+}
+
