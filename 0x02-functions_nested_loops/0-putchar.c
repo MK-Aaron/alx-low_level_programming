78e03bf88cc056fcd8a6192b59fda2e984e1aa43
MK-Aaron <ronimarvell@gmail.com>
2023-02-21 11:02:48 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char hello[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
+
+	printf("%s\n", hello);
+
+	return (0);
+}
