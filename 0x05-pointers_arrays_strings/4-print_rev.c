25ec16891f2147d5cc3f55804da1322f70e6f111
MK-Aaron <ronimarvell@gmail.com>
2023-02-27 10:00:56 -0800

    print in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ * @s: string to be reversed
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
