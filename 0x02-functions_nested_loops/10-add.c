d1916d95f56fbc9f976a168e2742ff9da4aa3fee
MK-Aaron <ronimarvell254@gmail.com>
2023-05-23 14:30:14 -0700

    Addition of intergeers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - addition 
+ * @a: arguement1
+ * @b: arguement2
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
