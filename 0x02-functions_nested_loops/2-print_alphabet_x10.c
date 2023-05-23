a25f73e3203721c29157bc37af9c588a3b7847ae
MK-Aaron <ronimarvell254@gmail.com>
2023-05-23 07:15:34 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: NULL
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			++ch;
+		}
+		_putchar('\n');
+	}
+}
