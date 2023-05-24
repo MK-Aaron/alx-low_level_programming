0b5c9c4c879d8435c8916fbb87c258442f41da1c
MK-Aaron <ronimarvell254@gmail.com>
2023-05-24 07:37:39 -0700

    Fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int i;
-	long int next = 0, tmp = 1, tmp2 = 2, sum = 3;
+	long int next = 0, tmp = 0, tmp2 = 1, sum = 0;
 
-	for (i = 3; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		next = tmp2 + tmp;
 		tmp = tmp2;
@@ -19,7 +19,8 @@ int main(void)
 
 		if (next > 4000000)
 			break;
-		sum += next;
+		else if (next % 2 == 0)
+			sum += next;
 	}
 	printf("%ld\n", sum);
 
