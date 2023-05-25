17c73b3ed11692fb6ff01a1575dd097041370e87
MK-Aaron <ronimarvell254@gmail.com>
2023-05-25 02:15:29 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Finds product of two arguements
+ * @a: arguemnt
+ * @b: arguement
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = 0;
+	sum = a * b;
+
+	return (sum);
+}
