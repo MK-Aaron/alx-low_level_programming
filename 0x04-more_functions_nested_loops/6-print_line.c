0856bd8f4f538701606bceb11057e1e39fd229a9
MK-Aaron <ronimarvell254@gmail.com>
2023-05-25 03:51:59 -0700

    prints lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - print horizontal lines
+ * @n: how long the line should be
+ *
+ * Return: Null
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	/* Checks if n is a digit */
+	if (!(_isdigit(n)))
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else if (n > 0)
+		{
+			i = 0;
+			while (i <= n)
+			{
+				_putchar('_');
+				i++;
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		printf("Error: 102\t%d\n", n);
+}
+
