a77bbfb5c64b752d47e3c90cfd288403ea77d3b2
MK-Aaron <ronimarvell254@gmail.com>
2023-05-25 02:37:54 -0700

    ommit 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints single digits omit 2 & 4
+ *
+ * return: Null
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
