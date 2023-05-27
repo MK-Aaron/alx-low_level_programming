e50b58bb7fa38ad31b43c53d9c9d8df8264e0c37
MK-Aaron <ronimarvell254@gmail.com>
2023-05-26 17:18:52 -0700

    Intergers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints intergers
+ * @n: interger to be printed
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
+}
