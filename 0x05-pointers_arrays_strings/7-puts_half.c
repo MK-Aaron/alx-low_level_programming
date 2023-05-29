84c59671ad1477a84187d6a19e0ccfc7842db4ed
ronigrep <ronimarvell254@gmail.com>
2023-05-29 09:15:25 -0700

    More strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half string
+ * @str: arguemnt to check
+ * Return: null
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	i = 0;
+	do {
+		i++;
+}
+	while (str[i] != '\0');
+
+	if (i % 2 != 0)
+	{
+		printf("%d", (i - 1) / 2);
+	}
+	else
+	{
+		i = i / 2;
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
