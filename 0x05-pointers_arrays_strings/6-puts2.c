1a0193db907be26fd721e6fa901695b86b1b467e
ronigrep <ronimarvell254@gmail.com>
2023-05-29 08:14:10 -0700

    String

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Prints a char then skips the other
+ * @str: String to print
+ * Return: Null
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
