4fcad67fa9b11acffd17937ddb1af82420a04286
ronigrep <ronimarvell254@gmail.com>
2023-05-29 08:33:58 -0700

    String

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,19 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
 	i = 0;
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		i++;
+	}
+
+	j = 0;
+	while (j != i / 2)
+	{
+		_putchar(str[j]);
+		j += 2;
 	}
 	_putchar('\n');
 }
