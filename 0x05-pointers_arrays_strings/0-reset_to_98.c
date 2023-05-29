52160aa0150f3487143d8eb09277d1dbee707c64
MK-Aaron <ronimarvell254@gmail.com>
2023-05-29 02:48:55 -0700

    Reset to 98x

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value 
+ * passed to 98.
+ * @n: pointer holding address to be changed
+ * Return: 0 Always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
