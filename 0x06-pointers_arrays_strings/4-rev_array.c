37335f125a5a7311937cfb9ba9b85cadd0048819
ronigrep <ronimarvell254@gmail.com>
2023-06-02 02:35:48 -0700

    array in rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - print in rev
+ * @a: array to reverse
+ * @n: Size of array
+ * Return: NULL
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp, j;
+
+	i = 0;
+	j = n;
+	while (i < (n / 2))
+	{
+		tmp = a[i];
+		a[i] = a[j - 1];/* a[j - 1] because we need only 13 characters */
+		a[j] = tmp;
+		i++;
+		j--;
+	}
+}
