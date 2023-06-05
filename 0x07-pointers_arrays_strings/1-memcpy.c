3d4480d6a86d51256255e3d6613429b7abd55dde
ronigrep <ronimarvell254@gmail.com>
2023-06-05 04:02:56 -0700

    Memcoptyx

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * @dest: location to copy to
  * @src: location to copy from
  * @n: number of bytes to copy
- * Retun: pointer
+ * Return: pointer
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -11,11 +11,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
-		*dest = src[i];
+		dest[i] = src[i];
 		i++;
-		dest++;
 	}
 
 	return (dest);
