4fa22d5adab6fc34f8871824ddbcc1e7deeddbaf
ronigrep <ronimarvell254@gmail.com>
2023-06-19 17:37:13 -0700

    Header-file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/* Prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
