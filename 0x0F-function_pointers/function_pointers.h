954d2718b154a678e1d9c2dd3979f476dea6647a
ronigrep <ronimarvell254@gmail.com>
2023-06-22 02:52:25 -0700

    Header File

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/******************** Header files *****************************/
+#include <stddef.h>
+#include <stdlib.h>
+
+/******************* Prototypes ********************************/
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
