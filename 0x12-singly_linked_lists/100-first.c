ae557c7677ddbf265bc6cc86942da19a909e706e
MK-Aaron <ronimarvell254@gmailcom>
2023-07-01 04:03:38 +0300

    Excute sub_func before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * before_main - execeutes before main function
+ */
+
+void before_main(void) __attribute__((constructor));
+
+void before_main(void)
+{
+	printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
