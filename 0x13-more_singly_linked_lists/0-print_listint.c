e2ae9d8549eca5d046b39a9b6579a0585aa94a9d
MK-Aaron <ronimarvell254@gmailcom>
2023-07-04 01:57:26 +0300

    Removed an  statement

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i, count = 0;
+	size_t count = 0;
 
 	while (h)
 	{
 		count++;
-		i = h->n;
 
-		if (i)
-			printf("%ld\n", i);
+		printf("%d\n", h->n);
 
 		h = h->next;
 	}
