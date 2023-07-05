212fc642acfcf0829cee53b787ce3e1d91210b41
MK-Aaron <ronimarvell254@gmailcom>
2023-07-05 03:10:45 +0300

    Free memory used by nodes

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees memory used by nodes
+ * @head: pointer to firat node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+
+		head = tmp;
+	}
+}
