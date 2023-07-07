32e1ec875a1b579dde7f73b9ad33940f904dfa4e
MK-Aaron <ronimarvell254@gmailcom>
2023-07-08 01:26:29 +0300

    Print binary '0'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,9 +7,23 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 0)
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
 	{
-		print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		printf("0");
+		return;
+	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
 	}
 }
