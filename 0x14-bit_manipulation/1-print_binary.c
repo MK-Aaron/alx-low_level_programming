560925950f16fbdd90c6aabad4a91ea92ab2e04d
MK-Aaron <ronimarvell254@gmailcom>
2023-07-08 01:03:37 +0300

    Fix:
    Header file - Typo
    Print_library - Recursion function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 {
 	if (n > 0)
 	{
-		printf(print_binary >> n);
+		print_binary(n >> 1);
 	}
 
 	_putchar((n & 1) + '0');
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,7 +8,7 @@
 /* Prototypes */
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-int _puchar(char c);
+int _putchar(char c);
 
 #endif
 
