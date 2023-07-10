fffa404e4db0538b8611abce9b58a5dfd6fd3c36
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-07-10 11:10:18 +0300

    Header File

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* C-Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
