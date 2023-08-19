c41ad22cd6d237675a8797ab92495d7d7b80961e
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-19 04:33:22 +0300

    Revert one commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,8 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 	}
 
-	while (tmp != NULL && i < idx)
+	while (i < idx - 1)
 	{
+		if (tmp == NULL)
+			return (NULL);
 		tmp = tmp->next;
 		i++;
 	}
