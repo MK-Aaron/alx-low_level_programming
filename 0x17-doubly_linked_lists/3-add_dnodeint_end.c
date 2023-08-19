b84f32a0294384fac2549d7cccee10e2ecf616e2
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-19 03:43:27 +0300

    fix: list to print in order

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,13 +14,26 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
-	new_node->next = *head;
-	if (*head != NULL)
-		(*head)->prev = new_node;
-	new_node->prev = NULL;
-	*head = new_node;
-	return (*head);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		dlistint_t *tmp = *head;
+
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_node;
+		new_node->prev = tmp;
+	}
+	return (new_node);
 }
