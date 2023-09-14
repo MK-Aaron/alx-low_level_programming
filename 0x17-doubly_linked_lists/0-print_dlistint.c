b417974f7c3141f731e6895c4b0f5f2b2aee0780
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-09-14 03:19:02 -0700

    Print doubly list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print elements in a list
+ * @h: pointer to first node
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int nod = 0;
+	while (h != NULL)
+	{
+		nod++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nod);
+}
